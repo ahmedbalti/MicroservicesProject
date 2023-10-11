73b10e83d5a8d637312ffb9e40106ad6d1f1a854
Ahmed <ahmed.balti@esprit.tn>
2023-10-11 23:08:17 +0100

    first push

---
+++ ServeurDecouverte/src/test/java/com/example/serveurdecouverte/ServeurDecouverteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serveurdecouverte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurDecouverteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
